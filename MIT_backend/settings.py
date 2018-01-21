c4b0d903dcbef1ac982709ae3f67f24c7663bcd1
Daw-Ran Liou <dawran6@gmail.com>
2018-01-20 21:31:07 -0800

    Add DRF-JWT as a dependency

--- MIT_backend/settings.py
+++ MIT_backend/settings.py
@@ -52,9 +52,14 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'MIT_backend.urls'
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-    )
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
 }
 
 TEMPLATES = [
