5132087541da08fbef4496ee1716db18fedcaee5
Daw-Ran Liou <dawran6@gmail.com>
2018-01-20 20:43:36 -0800

    Add test for account-create API

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+
+class AccountsTest(APITestCase):
+    def setUp(self):
+        self.test_user = User.objects.create_user('test', 'test@example.com', 'testpassword')
+        self.create_url = reverse('account-create')
+
+    def test_create_user_return_valid_token(self):
+        data = {
+            'username': 'foobar',
+            'email': 'foobar@example.com',
+            'password': 'somepassword',
+        }
+
+        response = self.client.post(self.create_url, data, format='json')
+
+        self.assertEqual(User.objects.count(), 2)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['username'], data['username'])
+        self.assertEqual(response.data['email'], data['email'])
+        self.assertFalse('password' in response.data)
