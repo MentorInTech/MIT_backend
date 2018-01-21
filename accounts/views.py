15d9f76e6eace249294ae0679d59c0a373941a31
Daw-Ran Liou <dawran6@gmail.com>
2018-01-20 21:23:37 -0800

    Create token when creating account

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authtoken.models import Token
 
 from .serializers import UserSerializer
 
@@ -15,6 +16,9 @@ class UserCreate(APIView):
         if serializer.is_valid():
             user = serializer.save()
             if user:
-                return Response(serializer.data, status.HTTP_201_CREATED)
+                token = Token.objects.create(user=user)
+                json = serializer.data
+                json['token'] = token.key
+                return Response(json, status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
