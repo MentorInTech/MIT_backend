97c086c5b00bd7cf7941eda59344debc7d5a186f
Daw-Ran Liou <dawran6@gmail.com>
2018-01-21 08:19:06 -0800

    Rearrange JWT urls under accounts.urls

--- MIT_backend/urls.py
+++ MIT_backend/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('accounts.urls')),
-    path('api-token-auth/', obtain_jwt_token),
 ]
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 from . import views
 
 urlpatterns = [
-    path('api/users', views.UserCreate.as_view(), name='account-create'),
+    path('register/', views.UserCreate.as_view(), name='account-create'),
+    path('login/', obtain_jwt_token),
+    path('token-verify/', verify_jwt_token),
+    path('token-refresh/', refresh_jwt_token),
 ]
