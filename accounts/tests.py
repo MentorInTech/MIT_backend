ab19e9a608fa3f45acf0d88abb85ed1dcbe8dd19
Daw-Ran Liou <dawran6@gmail.com>
2018-01-20 21:06:48 -0800

    Validate email when creating account

--- accounts/tests.py
+++ accounts/tests.py
@@ -88,3 +88,42 @@ class AccountsTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(User.objects.count(), 1)
         self.assertEqual(len(response.data['username']), 1)
+
+    def test_create_user_with_preexist_email(self):
+        data = {
+            'username': 'foobar',
+            'email': 'test@example.com',
+            'password': 'somepassword',
+        }
+
+        response = self.client.post(self.create_url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(len(response.data['email']), 1)
+
+    def test_create_user_with_invalid_email(self):
+        data = {
+            'username': 'foobar',
+            'email': 'testexample.com',
+            'password': 'somepassword',
+        }
+
+        response = self.client.post(self.create_url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(len(response.data['email']), 1)
+
+    def test_create_user_with_no_email(self):
+        data = {
+            'username': 'foobar',
+            'email': '',
+            'password': 'somepassword',
+        }
+
+        response = self.client.post(self.create_url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(len(response.data['email']), 1)
