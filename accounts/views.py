362cba3f3c8e9b3ba4e88906b72cab6b31012c03
Daw-Ran Liou <dawran6@gmail.com>
2018-01-26 10:39:22 -0800

    Implement email confirmation with local SMTP server

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,14 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.utils.encoding import force_text, force_bytes
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework.views import APIView
 
 from .serializers import UserSerializer
+from .tokens import account_activation_token
 
 
 class UserCreate(APIView):
@@ -20,7 +26,40 @@ class UserCreate(APIView):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
+            user.is_active = False
+            user.save()
             if user:
+                url = reverse('activate_account',
+                              kwargs={
+                                  'uidb64': urlsafe_base64_encode(force_bytes(user.pk)).decode(),
+                                  'token': account_activation_token.make_token(user)
+                              },
+                              request=request)
+                message = f'''Hi {user.username},
+Please click on the link to confirm your registration,
+
+{url}'''
+                send_mail('Confirm your email', message, from_email='localhost', recipient_list=[user.email])
                 return Response(serializer.data, status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+
+class ActivateAccount(APIView):
+    permission_classes = ()
+    authentication_classes = ()
+
+    def get(self, request, uidb64, token):
+        try:
+            uid = force_text(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
+
+        if user is not None and account_activation_token.check_token(user, token):
+            user.is_active = True
+            user.save()
+            return Response('Thank you for your email confirmation. Now you can login your account.',
+                            status.HTTP_200_OK)
+        else:
+            return Response('Activation link is invalid!', status.HTTP_400_BAD_REQUEST)
