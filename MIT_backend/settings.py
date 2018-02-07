1cd840959d2a20f0338fdc7fd52f830c8f7d9da8
Daw-Ran Liou <dawran6@gmail.com>
2018-02-06 22:03:04 -0800

    Add swagger API doc generator

--- MIT_backend/settings.py
+++ MIT_backend/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
 
+    'rest_framework_swagger',
+
     'accounts',
 ]
 
--- MIT_backend/urls.py
+++ MIT_backend/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_swagger.views import get_swagger_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('accounts.urls')),
+
+    path('docs/', get_swagger_view(title='Mentor In Tech API')),
 ]
