39cf35b8f0c89170446944bc3d0372d0facf200b
Daw-Ran Liou <dawran6@gmail.com>
2018-05-15 19:50:56 -0700

    Fix travis

--- config/settings/test.py
+++ config/settings/test.py
@@ -1,1 +1,13 @@
 from .base import *  # noqa
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_env_variable('DATABASE_NAME'),
+        'USER': get_env_variable('DATABASE_USER'),
+        'PASSWORD': get_env_variable('DATABASE_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
